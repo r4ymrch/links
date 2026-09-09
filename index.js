55468fe1ba95e108831fd041a66d468f4aab88d4
r4ymrch <rymr.ch14@gmail.com>
2026-09-09 23:32:57 +0700

    feat: first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+document.getElementById("gh-btn").addEventListener("click", function () {
+   setTimeout(function () {
+      window.open(`https://github.com/r4ymrch`, "_blank");
+   }, 300);
+});
+
+document.getElementById("ig-btn").addEventListener("click", function () {
+   setTimeout(function () {
+      window.open(`https://www.instagram.com/radityo.h`, "_blank");
+   }, 300);
+});
+
+document.getElementById("ig-lbtn").addEventListener("click", function () {
+   setTimeout(function () {
+      window.open(`https://www.instagram.com/radityo.h`, "_blank");
+   }, 300);
+});
+
+document.getElementById("x-btn").addEventListener("click", function () {
+   setTimeout(function () {
+      window.open(`https://x.com/r4ymrch14`, "_blank");
+   }, 300);
+});
+
+document.getElementById("x-lbtn").addEventListener("click", function () {
+   setTimeout(function () {
+      window.open(`https://x.com/r4ymrch14`, "_blank");
+   }, 300);
+});
+
+document.getElementById("gh-repo-lbtn").addEventListener("click", function () {
+   setTimeout(function () {
+      window.open(`https://github.com/r4ymrch?tab=repositories`, "_blank");
+   }, 300);
+});
