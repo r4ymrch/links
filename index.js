0dc61246d27774f7845d8c163536ae239912a558
r4ymrch <rymr.ch14@gmail.com>
2026-09-10 12:37:04 +0700

    feat: major changes, add text animation, more link, automatic profile picture, and several improvement

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// link to
 var delay = 300;
 var links = [
    { id: "gh", url: "https://github.com/r4ymrch" },
@@ -12,6 +13,7 @@ var links = [
    { id: "fb-2", url: "https://www.facebook.com/share/19Ek4Mcgoc/" },
    { id: "wa", url: "https://wa.me/62895622345999" },
    { id: "email", url: "mailto:rymr.ch14@gmail.com" },
+   { id: "yt", url: "https://www.youtube.com/@r4ymrch" },
    { id: "ig", url: "https://www.instagram.com/radityo.h" }
 ];
 
@@ -30,3 +32,37 @@ links.forEach((item) => {
       });
    }
 });
+
+// animasi text
+const textElement = document.getElementById("username");
+const words = ["r4ymrch", "Adnan Naufal"];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeLoop() {
+  const currentWord = words[wordIndex];
+  if (isDeleting) {
+    textElement.textContent = currentWord.substring(0, charIndex - 1);
+    charIndex--;
+  } else {
+    textElement.textContent = currentWord.substring(0, charIndex + 1);
+    charIndex++;
+  }
+
+  let typeSpeed = isDeleting ? 100 : 200; // ms
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    typeSpeed = 3500; // delay antar kalimat
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    typeSpeed = 500;
+  }
+
+  setTimeout(typeLoop, typeSpeed);
+}
+
+document.addEventListener("DOMContentLoaded", typeLoop);
