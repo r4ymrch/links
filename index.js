8893ed6984533598f08c4dc55d8500e7e1578be9
r4ymrch <rymr.ch14@gmail.com>
2026-09-10 10:37:26 +0700

    feat: major changes, remove nerd fonts and using svg sprite instead, activate some links

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-document.getElementById("gh-btn").addEventListener("click", function () {
-   setTimeout(function () {
-      window.open(`https://github.com/r4ymrch`, "_blank");
-   }, 300);
-});
+var delay = 300;
+var links = [
+   { id: "gh", url: "https://github.com/r4ymrch" },
+   { id: "li", url: "https://www.linkedin.com/in/adnan-naufal-a88806435" },
+   { id: "li-2", url: "https://www.linkedin.com/in/adnan-naufal-a88806435" },
+   { id: "repo", url: "https://github.com/r4ymrch?tab=repositories" },
+   { id: "x", url: "https://x.com/r4ymrch14" },
+   { id: "x-2", url: "https://x.com/r4ymrch14" },
+   { id: "dc", url: "https://discord.com/users/r4ymrch_" },
+   { id: "dc-2", url: "https://discord.com/users/r4ymrch_" },
+   { id: "fb", url: "https://www.facebook.com/share/19Ek4Mcgoc/" },
+   { id: "fb-2", url: "https://www.facebook.com/share/19Ek4Mcgoc/" },
+   { id: "wa", url: "https://wa.me/62895622345999" },
+   { id: "email", url: "mailto:rymr.ch14@gmail.com" },
+   { id: "ig", url: "https://www.instagram.com/radityo.h" }
+];
 
-document.getElementById("ig-btn").addEventListener("click", function () {
-   setTimeout(function () {
-      window.open(`https://www.instagram.com/radityo.h`, "_blank");
-   }, 300);
-});
+links.forEach((item) => {
+   const element = document.getElementById(item.id);
 
-document.getElementById("ig-lbtn").addEventListener("click", function () {
-   setTimeout(function () {
-      window.open(`https://www.instagram.com/radityo.h`, "_blank");
-   }, 300);
-});
-
-document.getElementById("x-btn").addEventListener("click", function () {
-   setTimeout(function () {
-      window.open(`https://x.com/r4ymrch14`, "_blank");
-   }, 300);
-});
-
-document.getElementById("x-lbtn").addEventListener("click", function () {
-   setTimeout(function () {
-      window.open(`https://x.com/r4ymrch14`, "_blank");
-   }, 300);
-});
-
-document.getElementById("gh-repo-lbtn").addEventListener("click", function () {
-   setTimeout(function () {
-      window.open(`https://github.com/r4ymrch?tab=repositories`, "_blank");
-   }, 300);
+   if (element) {
+      element.addEventListener("click", function () {
+         if (item.url.startsWith("mailto:")) {
+            window.location.href = item.url;
+         } else {
+            setTimeout(function () {
+               window.location.href = item.url;
+            }, delay);
+         }
+      });
+   }
 });
